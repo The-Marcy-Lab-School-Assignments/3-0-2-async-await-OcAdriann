31c17829a7d0c1fd6fe80ebe79053cb816838334
OcAdriann <ocadriannnc20@gmail.com>
2024-06-20 12:55:10 -0400

    code is done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,2 +1,25 @@
-export const fetchHandler = () => {
+export const fetchHandler = async (url, option = {}) => {
+
+    try {
+        const response = await fetch(url, option)
+
+        if(!response.ok) throw new Error(`Fetch failed. ${response.status} ${response.statusText}`)
+            
+        const isJson = (response.headers.get('content-type') || '').includes('application/json');
+
+        if(isJson) {
+            const jsonData = await response.json()
+            return [jsonData, null]
+        } else {
+            const textData = await response.text()
+            return [textData, null]
+        }
+    }
+
+    catch(error) {
+        console.warn(error)
+        return [null, error]
+    }
+    
+    return [{}, null]
 };
